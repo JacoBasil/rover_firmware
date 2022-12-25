53f8d5ec0fc66b14020206da752e733c961373c7
Jetson nano <yakov.vasilev.95@mail.ru>
2022-12-26 00:31:59 +0300

    ignore build

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jetson/rover_ws/src/rover_firmware
-# Build directory: /home/jetson/rover_ws/src/rover_firmware/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
